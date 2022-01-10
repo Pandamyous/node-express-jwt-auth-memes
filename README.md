888f919824c7aa624e9ca7fa94c2795439d2538a
Pandamyous <255782@mail.muni.cz>
2022-01-10 17:25:18 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Random-Memes
+
+This project serves just for learning purposes.
+Its a small application using ejs for front-end while mainly focusing on backend.
+In Backend i was testing authentication via sending jwt tokens in cookies.
+The application can be also run in docker. 
+The reason for Dockerfile and docker-compose was to try and have local dockerfile,
+which is read and build with docker-compose and then adding mongoDB to it as well. I know it can be done easier, I just wanted to test it :P
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+// docker compose mongoDB local db
+const dbURI = 'mongodb://anton:jurca@mongodb';
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// routes
+app.get('*', checkUser);
+app.get('/', (req, res) => res.render('home'));
+app.get('/memes', requireAuth, (req, res) => res.render('memes'));
+app.use(authRoutes);
+
